81131db91f801ef5774f129d2c33aacf14ba216c
tomidev <debreuiltomas@gmail.com>
2023-10-19 14:16:52 -0300

    trabajo en vista de productos

--- app/components/Product.js
+++ app/components/Product.js
@@ -1,12 +1,14 @@
 import { fetch_request } from "../helpers/fetch_request.js";
-const d = document,
-  $productContainer = d.createElement("div"),
-  $product = d.createElement("article");
-
-$productContainer.classList.add("product-content");
-$product.classList.add("product-container");
+import { Auth } from "../helpers/authCheck.js";
 
 export function Product() {
+  const d = document,
+    $productContainer = d.createElement("div"),
+    $product = d.createElement("article");
+
+  $productContainer.classList.add("product-content");
+  $product.classList.add("product-container");
+
   let ProductCard = async () => {
     const currentHash = window.location.hash;
     const productIdMatch = currentHash.match(/#\/product\/(\d+)/);
@@ -16,7 +18,7 @@ export function Product() {
       cbSuccess: async (product) => {
         $productContainer.innerHTML = `
         <div class="product-title-container">
-          <p class="product-title">${product.title}</p>
+          <p class="product-title-prod">${product.title}</p>
         </div>
         <div class="product-img-container">
             <img src="${product.image}" alt="${
@@ -31,29 +33,28 @@ export function Product() {
               <p>${product.description}</p>
             </div>
             <div class="add-cart-btn-container">
-              <button class="add-cart-btn">Agregar al carrito</button>
+              <button class="add-cart-btn" id="add-cart-btn-${
+                product.id
+              }">Agregar al carrito</button>
               </div>
         </div>
         `;
+
+        d.getElementById("main").addEventListener("click", (e) => {
+          if (e.target.matches(`#add-cart-btn-${product.id}`)) {
+            Auth.userDb.addProductToCart(
+              product.id,
+              product.title,
+              product.price,
+              product.image
+            );
+          }
+        });
       },
     });
-
     $product.appendChild($productContainer);
   };
 
-  /*
-  const currentHash = window.location.hash;
-  const productIdMatch = currentHash.match(/#\/product\/(\d+)/); // Esto buscará "#/product/" seguido de un número (el ID del producto)
-  const $product = document.createElement("div");
-  $product.classList.add("producto");
-  if (productIdMatch) {
-    const productId = productIdMatch[1];
-    console.log(productId);
-    $product.innerHTML = `
-  <h2>${productId}</h2>
-  `;
-  }
-*/
   ProductCard();
   return $product;
 }
