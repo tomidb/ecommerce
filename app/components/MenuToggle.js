20739b2fc671c4508fb0b2d8ee23ab5c938afa79
tomidev <debreuiltomas@gmail.com>
2023-11-14 23:46:59 -0300

    primera correccion de menu

--- app/components/MenuToggle.js
+++ app/components/MenuToggle.js
@@ -1,16 +1,22 @@
 export function MenuToggle() {
-  const $menuToggle = document.createElement("label");
+  const $menuToggle = document.createElement("div");
 
   $menuToggle.classList.add("menu-toggle");
   $menuToggle.innerHTML = `
   <span class="menu-toggler-bar"></span>
   <span class="menu-toggler-bar-mid"></span>
   <span class="menu-toggler-bar"></span>
+  <span class="menu-toggle-mask"></span>
   `;
 
   $menuToggle.addEventListener("click", (e) => {
-    if (e.target.matches(".menu-toggle")) {
+    if (e.target.matches(".menu-toggle-mask")) {
       document.querySelector(".navbar").classList.toggle("display-toggle");
+      if (
+        !document.querySelector(".children").classList.contains("display-none")
+      ) {
+        document.querySelector(".children").classList.toggle("display-none");
+      }
     }
   });
 
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,19 +4,19 @@ export function Navbar() {
 
   $navbar.addEventListener("click", (e) => {
     if (e.target.matches(".categories-link")) {
-      document.querySelector(".children").classList.add("display-none");
+      document.querySelector(".children").classList.toggle("display-none");
       console.log(e.target);
     }
   });
 
   $navbar.innerHTML = `
-<ul>
+<ul class="navbar-menu">
 				<li><a href="#">Volver a Inicio</a></li>
 				<li class="submenu">
-					<a href="#" class="categories-link">Categorías</a>
-					<ul class="children">
+					<span class="categories-link">Categorías<i class="fa-solid fa-caret-down arrow-down"></i></span>
+					<ul class="children display-none">
 						<li><a href="#/category/Mouse">Mouse</a></li>
-						<li><a href="#/category/Teclado">Teclado </a></li>
+						<li><a href="#/category/Teclado">Teclado</a></li>
 						<li><a href="#/category/Notebook">Notebook</a></li>
             <li><a href="#/category/Monitor">Monitor</a></li>
 					</ul>
